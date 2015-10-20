b82a6e8bdbd81a05042e80c5e00558c3ef4d6cd9
Paulo Gama <gamaufla@gmail.com>
2015-10-19 23:15:00 -0200

    Facebook  Login Graph

--- app/src/main/java/br/com/sanduba/sanduba/MainActivity.java
+++ app/src/main/java/br/com/sanduba/sanduba/MainActivity.java
@@ -1,16 +1,81 @@
 package br.com.sanduba.sanduba;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class MainActivity extends FragmentActivity {
+
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+    private TextView greeting;
+    private static final String PERMISSION = "public_profile, email, user_birthday";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+        new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Success2","Success2");
+            }
+
+            @Override
+            public void onCancel() {
+                showAlert();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                showAlert();
+            }
+
+            private void showAlert() {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.cancelled)
+                        .setMessage(R.string.permission_not_granted)
+                        .setPositiveButton(R.string.ok, null)
+                        .show();
+            }
+
+        });
+
         setContentView(R.layout.activity_main);
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                updateUI();
+            }
+        };
+
+        greeting = (TextView) findViewById(R.id.greeting);
     }
 
     @Override
@@ -33,5 +98,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
+    private void updateUI() {
+        boolean enableButtons = AccessToken.getCurrentAccessToken() != null;
+
+        LoginManager.getInstance().logInWithReadPermissions(this,Arrays.asList(PERMISSION));
+
+        Profile profile = Profile.getCurrentProfile();
+
+        if (enableButtons && profile != null) {
+            greeting.setText(getString(R.string.hello_user, profile.getFirstName()));
+            GraphRequest request = GraphRequest.newMeRequest(
+                    AccessToken.getCurrentAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(JSONObject object,GraphResponse response) {
+                            Log.v("LoginActivity", response.toString());
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,email,gender, birthday");
+            request.setParameters(parameters);
+            request.executeAsync();
+        } else {
+            greeting.setText(null);
+        }
     }
 }
